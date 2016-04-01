7b367e3d991645b1870663ab8fe920077e08e983
sabby918 <skst4b@mail.umkc.edu>
2016-03-31 19:40:55 -0500

    Update Building.h

--- ElevatorProject/Building.h
+++ ElevatorProject/Building.h
@@ -7,18 +7,19 @@ using namespace std;
 
 class Elevator {
 private:
-	int ID, current, direction;
+	int ID, current, floornumber;
+	int direction = -1;
 	bool up;
 	bool moving;
 public:
 	list<int> destinations;
 	void addDestination(int floor);
-	void moveUp() { ++current; checkDestinations(); };
-	void moveDown() { --current; checkDestinations(); };
+	void moveUp() {if (current < floornumber) {++current; checkDestinations(); } };
+	void moveDown() {if (current > 0) {--current; checkDestinations(); } };
 	void checkDestinations();
 	void changeDirection();
 	void move();
-	void setID(int id) { ID = id; };
+	void setID(int id, int floor) { ID = id, floornumber = floor; };
 	int getLevel() { return current; }
 
 };
@@ -26,16 +27,21 @@ public:
 class building {
 	friend class elevator;
 private:
-	struct call { int floor, goal, direction; bool pickedUP; };
+	struct call { int floor, goal, direction; bool pickedUP, arrived; };
 	list<call> floorCall;
 	vector<Elevator> elevators;
 	int elevatorNumber;
 	int floorNumbers;
+	int completeCalls = 0;
 public:
+	list<call>::iterator location = floorCall.begin();
+	void setLocation(list<call>::iterator here);
+	int getComplete() { return completeCalls; }
 	void generate();
 	void setElevator(int number = 1);
 	void setFloors(int floors = 10);
+	void dropOff(list<call> requests);
 	void moveCalls();
-	void moveElevators();
+	int moveElevators();
 	void simulate();
 };
