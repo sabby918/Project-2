b407ed9cf85be20ff56159ab4ec7f8138fbd4af5
sabby918 <skst4b@mail.umkc.edu>
2016-04-06 16:47:32 -0500

    option 2 needs fixing// and average time is wrong

---
+++ Building.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <vector>
+#include <list>
+
+
+using namespace std;
+
+class elevator {
+private:
+	int current, floornumber;
+	bool up;
+	bool moving;
+public:
+	// log of where the elevator will go
+	list<int> destinations;
+	int direction = -1;
+	void restart(int number) { current = number; }
+	void setMoving(bool yes_no) { moving = yes_no; }
+	bool isMoving() { return moving; }
+	void addDestination(int floor);
+	void moveUp() {if (current < floornumber) {++current; } };
+	void moveDown() {if (current > 0) {--current; } };
+	void checkDestinations();
+	void changeDirection();
+	void move();
+	void setNumFloor(int floor) {floornumber = floor; };
+	int getLevel() { return current; }
+
+};
+
+class building {
+	friend class elevator;
+private:
+
+	struct call { int floor, goal, direction, waitingTime; bool pickedUP, arrived; };
+
+	// list of all the calls that are generated for that round
+	list<call> floorCall;
+	elevator theElevator;
+	int elevatorNumber;
+
+	//total number of floors in the building
+	int floorNumbers;
+	int completeCalls = 0;
+	int roundTime = 0;
+public:
+	//where are we in the floorCall list
+	list<call>::iterator location = floorCall.begin();
+	void setLocation(list<call>::iterator here);
+	int getComplete() { return completeCalls; }
+	void calcWaitingTime();
+
+	//creates a random call
+	void generate();
+	void generate(int numfloors);
+	bool searchDest(int target, list<int> theList);
+	bool searchCall(int target, list<call> theList);
+	void setElevator(int number = 1);
+	void setFloors(int floors = 10);
+	void dropOff(list<call> requests,ostream& file);
+	void moveCalls();
+	int moveElevators();
+	void simulate();
+	void pickup();
+	int dropoff(int num);
+	int createCalls(int requests, int percentage, int possiblePeople);
+};
---
+++ Elevator.cpp
@@ -0,0 +1,100 @@
+#include "building.h"
+
+
+
+void elevator::addDestination(int floor) {
+	list<int>::iterator itr;
+	moving = true;
+	if (destinations.empty()) {
+		destinations.push_back(floor);
+		if ((current - floor) > 0) {
+			up = false;
+			direction = 0;
+		}
+		else {
+			up = true;
+			direction = 1;
+		}
+
+
+		return;
+	}
+	for (itr = destinations.begin(); itr != destinations.end(); itr++) {
+		if (floor < *itr) {
+			destinations.insert(itr, floor);
+			return;
+		}
+	}
+	destinations.push_back(floor);
+	return;
+}
+
+void elevator::checkDestinations() {
+	list<int>::iterator itr;
+	if (destinations.empty())
+		return;
+	for (itr = destinations.begin(); itr != destinations.end(); itr++) {
+		if (*itr == current) {
+	
+			return;
+		}
+	}
+}
+
+void elevator::move() {
+
+	changeDirection();
+
+	
+	
+	if (destinations.empty()) {
+		moving = false;
+		up = false;
+	}
+	if (moving && up) {
+		moveUp();
+		return;
+	}
+	if (moving && !up) {
+		moveDown();
+		return;
+	}
+	else {
+		return;
+	}
+}
+
+
+void elevator::changeDirection() {
+	list<int>::iterator itr;
+	if (up) {
+		bool greater = false;
+		for (itr = destinations.begin(); itr != destinations.end(); itr++) {
+			if (current > *itr) {
+				greater = true;
+				up = false;
+			}
+			else {
+				greater = false;
+				up = true;
+				return;
+			}
+		}
+		return;
+	}
+	else {
+		bool greater = true;
+		for (itr = destinations.begin(); itr != destinations.end(); itr++) {
+			if (current < *itr) {
+				greater = false;
+				up = true;
+			}
+			else {
+				greater = true;
+				up = false;
+				return;
+			}
+		}
+		return;
+	}
+}
